b258c1775b5d0a1b76404f76a0a5b7ea06157c2c
lukethanks <lukethanks@gmail.com>
2020-10-21 20:43:50 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component, VERSION } from '@angular/core';
+function log(target, name, descriptor){
+  console.log(target, name, descriptor)
+  const original = descriptor.value
+
+  descriptor.value=fuction(){
+    console.log("This function was hacked!")
+  }
+  return descriptor
+}
 
 @Component({
   selector: 'my-app',
@@ -7,4 +16,12 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Svarti ' + VERSION.major;
+constructor(){
+  this.aSimpleMethod()
+}
+
+
+@log
+aSimpleMethod()
+{console.log("Hey there!")}
 }
